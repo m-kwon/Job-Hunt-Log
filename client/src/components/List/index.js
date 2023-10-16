c66bacc38d4d5bd6402f36aaaf733f40a58a73e5
Matthew Kwon <matthew.o.kwon@gmail.com>
2023-10-16 11:22:08 -0700

    Created a modal window for adding job applications

--- client/src/components/List/index.js
+++ client/src/components/List/index.js
@@ -1,10 +1,14 @@
 import React, { useState } from 'react';
+import Modal from 'react-modal';
 import { useDispatch } from 'react-redux';
 import { addApplication } from '../../slices/jobApplicationsSlice';
 
 import './styles.css';
 
+Modal.setAppElement('#root');
+
 const List = ({ listName }) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   const [newJobData, setNewJobData] = useState({
     title: '',
     company: '',
@@ -21,11 +25,20 @@ const List = ({ listName }) => {
     });
   };
 
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
   const addCard = () => {
     if (newJobData.title.trim() !== '' && newJobData.company.trim() !== '' && newJobData.jobUrl.trim() !== '') {
       const applicationDate = new Date().toISOString();
-      dispatch(addApplication({ ...newJobData, applicationDate }));
+      dispatch(addApplication({ ...newJobData, applicationDate, listName }));
       setNewJobData({ title: '', company: '', jobUrl: '' });
+      closeModal();
     }
   };
 
@@ -33,31 +46,40 @@ const List = ({ listName }) => {
     <div className="list">
       <div className="list-header">
         {listName}
-        <button className="add-button" onClick={addCard}>
+        <button className="add-button" onClick={openModal}>
           +
         </button>
       </div>
-      <input
-        type="text"
-        name="title"
-        value={newJobData.title}
-        onChange={handleInputChange}
-        placeholder="Job Title"
-      />
-      <input
-        type="text"
-        name="company"
-        value={newJobData.company}
-        onChange={handleInputChange}
-        placeholder="Company Name"
-      />
-      <input
-        type="text"
-        name="jobUrl"
-        value={newJobData.jobUrl}
-        onChange={handleInputChange}
-        placeholder="Job Listing URL"
-      />
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        contentLabel="Add Job Application Modal"
+      >
+        <h2>Add Job Application</h2>
+        <input
+          type="text"
+          name="title"
+          value={newJobData.title}
+          onChange={handleInputChange}
+          placeholder="Job Title"
+        />
+        <input
+          type="text"
+          name="company"
+          value={newJobData.company}
+          onChange={handleInputChange}
+          placeholder="Company Name"
+        />
+        <input
+          type="text"
+          name="jobUrl"
+          value={newJobData.jobUrl}
+          onChange={handleInputChange}
+          placeholder="Job Listing URL"
+        />
+        <button onClick={addCard}>Add Card</button>
+        <button onClick={closeModal}>Cancel</button>
+      </Modal>
     </div>
   );
 };
