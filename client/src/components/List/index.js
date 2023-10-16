775138842af1892bc4d6029b2ac0f024635f6848
Matthew Kwon <matthew.o.kwon@gmail.com>
2023-10-16 11:18:42 -0700

    Created a form for users to submit data that adheres to the db schema

--- client/src/components/List/index.js
+++ client/src/components/List/index.js
@@ -1,20 +1,63 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import Card from '../Card';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addApplication } from '../../slices/jobApplicationsSlice';
 
 import './styles.css';
 
 const List = ({ listName }) => {
-  const cards = useSelector((state) => state.jobApplications.applications.filter((app) => app.list === listName));
+  const [newJobData, setNewJobData] = useState({
+    title: '',
+    company: '',
+    jobUrl: '',
+  });
+
+  const dispatch = useDispatch();
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setNewJobData({
+      ...newJobData,
+      [name]: value,
+    });
+  };
+
+  const addCard = () => {
+    if (newJobData.title.trim() !== '' && newJobData.company.trim() !== '' && newJobData.jobUrl.trim() !== '') {
+      const applicationDate = new Date().toISOString();
+      dispatch(addApplication({ ...newJobData, applicationDate }));
+      setNewJobData({ title: '', company: '', jobUrl: '' });
+    }
+  };
 
   return (
     <div className="list">
-      <h2>{listName}</h2>
-      <div className="card-list">
-        {cards.map((card) => (
-          <Card key={card.id} card={card} />
-        ))}
+      <div className="list-header">
+        {listName}
+        <button className="add-button" onClick={addCard}>
+          +
+        </button>
       </div>
+      <input
+        type="text"
+        name="title"
+        value={newJobData.title}
+        onChange={handleInputChange}
+        placeholder="Job Title"
+      />
+      <input
+        type="text"
+        name="company"
+        value={newJobData.company}
+        onChange={handleInputChange}
+        placeholder="Company Name"
+      />
+      <input
+        type="text"
+        name="jobUrl"
+        value={newJobData.jobUrl}
+        onChange={handleInputChange}
+        placeholder="Job Listing URL"
+      />
     </div>
   );
 };
