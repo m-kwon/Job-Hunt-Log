51427f81c68e06b6593d81a513a47cac0c9a35b5
Matthew Kwon <matthew.o.kwon@gmail.com>
2023-10-16 09:15:20 -0700

    Updated schema

--- server/database.sql
+++ server/database.sql
@@ -1,11 +1,11 @@
 CREATE DATABASE "Job-Hunt-Log";
 
-CREATE SCHEMA IF NOT EXISTS job_applications;
+CREATE SCHEMA IF NOT EXISTS job_hunt_log;
 
-CREATE TABLE IF NOT EXISTS job_applications.applications (
+CREATE TABLE IF NOT EXISTS job_hunt_log.applications (
   id SERIAL PRIMARY KEY,
-  job_title VARCHAR(255) NOT NULL,
-  company_name VARCHAR(255) NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  company VARCHAR(255) NOT NULL,
   job_url VARCHAR(255),
   application_date DATE
 );
